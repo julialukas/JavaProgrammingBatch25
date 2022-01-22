d4bcde4feb444abdc0f65a269b3fd564475e5022
julialukas <juliashea@ymail.com>
2022-01-22 11:08:54 -0500

    day6 Avenger Practice Hour

---
+++ AvengerPracticeHour/Adam_22.java
@@ -0,0 +1,11 @@
+public class Adam_22 {
+
+    public class Room{
+        double width;
+        double length;
+        public String print(){ return "Width:"+ width + ",Length:" + length;}
+        public void setInfo(double width, double length){
+
+        }
+    }
+}
